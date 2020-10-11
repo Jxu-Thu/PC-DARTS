12ec5c18ce3836739422d2cd23aaa52f807f6ee1
Jxu-Thu <jxu3425@gmail.com>
2020-10-11 10:49:08 +0800

    1.2.0

--- train_search.py
+++ train_search.py
@@ -19,8 +19,8 @@ from architect import Architect
 
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--set', type=str, default='cifar10', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='/blob/v-jinx/data', help='location of the data corpus')
+parser.add_argument('--set', type=str, default='cifar10_pc_darts', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=256, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.1, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
